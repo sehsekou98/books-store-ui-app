ee75c57059c329d92891f33f1d05ac127f623911
Sekou S Seh <sehsekou98@gmail.com>
2024-05-09 04:46:09 +0100

    update

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthenticationRequest } from '../../services/models';
+import {  Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +10,37 @@ import { AuthenticationRequest } from '../../services/models';
 })
 export class LoginComponent {
 
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+  ) {}
+
   authRequest: AuthenticationRequest = {email: '', password: ''};
   errorMsg: Array<string> = [];
 
   register() {
+    this.router.navigate(['register']);
 
     }
-    
+
     login() {
+      this.errorMsg = [];
+      this.authService.authenticate({
+        body: this.authRequest
+      }).subscribe({
+        next: () => {
+          this.router.navigate(['book']);
+        },
+        error: (err) => {
+          console.log(err);
+          if(err.error.validationErrors) {
+            this.errorMsg = err.error.validationErrors;
+          }else {
+            this.errorMsg.push(err.console.error.error);
+            
+          }
+        }
+      });
 
     }
 
