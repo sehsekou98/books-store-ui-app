ada4c45b647ca0b5ba8f89cf077b0526b4d2001f
Sekou S Seh <sehsekou98@gmail.com>
2024-05-10 02:51:01 +0100

    book-listing

---
+++ src/app/modules/book/pages/book-list/book-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { borrowBook } from '../../../../services/fn/book/borrow-book';
+import { BookService } from '../../../../services/services';
+import { Router } from '@angular/router';
+import { findAllBooks } from '../../../../services/fn/book/find-all-books';
+import { PageResponseBookResponse } from '../../../../services/models';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrl: './book-list.component.scss'
+})
+export class BookListComponent implements OnInit{
+  bookResponse: PageResponseBookResponse = {};
+   page: number = 0;
+   size: number = 5;
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {}
+
+
+  ngOnInit(): void {
+   this.findAllBooks();
+  }
+  private findAllBooks() {
+this.bookService.findAllBooks( {
+  page: this.page,
+  size: this.size
+}).subscribe({
+  next: (books) => {
+this.bookResponse = books;
+  }
+})
+  }
+
+}
